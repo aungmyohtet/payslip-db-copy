fabcfa31c0ae1d48804cd7b7312548fc57690a12
hninnayecho <hninn.aye.cho@frobom.com>
2017-11-20 15:07:01 +0630

    new_row_mapper

---
+++ src/main/java/com/frobom/payslip/rowmapper/PositionRowMapper.java
@@ -0,0 +1,24 @@
+package com.frobom.payslip.rowmapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import com.frobom.payslip.entity.Position;
+
+public class PositionRowMapper implements RowMapper<Position>{
+
+    @Override
+    public Position mapRow(ResultSet rs, int arg1) throws SQLException {
+        Position position = new Position();
+        position.setCreatedDate(rs.getDate("created"));
+        position.setDeleted(rs.getString("deleted"));
+        position.setModifiedDate(rs.getDate("modified"));
+        position.setName(rs.getString("position_name"));
+        position.setCompanyId(rs.getInt("company_id"));
+        position.setId(rs.getInt("id"));
+        return position;
+    }
+
+}
